67f42fd182c7bc3a5197cdd31de6bffc4f331850
Riski Bambela <riskibambela01@gmail.com>
2026-09-25 17:07:20 +0800

    Update feedback.js

--- api/feedback.js
+++ api/feedback.js
@@ -234,7 +234,7 @@ export default async function handler(req, res) {
     "🏆 PAK LUA VIP MOD BAN 🏆\n" +
     "🔥 AUTO FEEDBACK 🔥\n" +
     "🦠 Bahan: AUTOFEEDBACK V9\n" +
-    "PUBG : " +
+    " : " +
     htmlEscape(pubgDisplay) +
     "\n" +
     "🎮 Versi: " +
