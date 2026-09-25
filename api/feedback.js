af162eeba6e47d832fe98271f5852beddfa86080
Riski Bambela <riskibambela01@gmail.com>
2026-09-25 11:22:39 +0800

    Update feedback version from V10 to V9

--- api/feedback.js
+++ api/feedback.js
@@ -234,7 +234,7 @@ export default async function handler(req, res) {
     "╚═══━━━─── • ───━━━═══╝\n" +
     "🏆 PAK LUA VIP MOD BAN 🏆\n" +
     "🔥 AUTO FEEDBACK 🔥\n" +
-    "🦠 Bahan: AUTOFEEDBACK V10\n" +
+    "🦠 Bahan: AUTOFEEDBACK V9\n" +
     htmlEscape(pubgDisplay) +
     "\n" +
     "🎮 Versi: " +
